acc65124da262e91f9b3e338dcdea6e0a7e12efa
Roman Gurov <roman.gurovik@gmail.com>
2025-05-08 10:22:07 +0700

    fix project

--- lua/roman/plugins/project.lua
+++ lua/roman/plugins/project.lua
@@ -3,7 +3,23 @@ return {
 	lazy = false,
 	config = function()
 		require("project_nvim").setup({
+			active = true,
+
+			on_config_done = nil,
+
+			manual_mode = false,
+
+			detection_methods = { "pattern" },
+
 			patterns = { ".git", "go.mod" },
+
+			show_hidden = false,
+
+			silent_chdir = true,
+
+			ignore_lsp = {},
+
+			datapath = vim.fn.stdpath("data"),
 		})
 	end,
 }
